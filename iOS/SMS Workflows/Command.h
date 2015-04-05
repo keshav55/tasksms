6d9a7eeb7afbda21e141a0221a35e69732b14920
Adrien Truong <adrien.truong@me.com>
2015-04-05 08:33:49 -0700

    update server

---
+++ iOS/SMS Workflows/Command.h
@@ -0,0 +1,13 @@
+//
+//  Command.h
+//  SMS Workflows
+//
+//  Created by Adrien on 4/5/15.
+//  Copyright (c) 2015 Adrien Truong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Command : NSObject
+
+@end
---
+++ iOS/SMS Workflows/CommandTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  CommandTableViewCell.h
+//  SMS Workflows
+//
+//  Created by Adrien on 4/5/15.
+//  Copyright (c) 2015 Adrien Truong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CommandTableViewCell : UITableViewCell
+
+@end
